56e12e8e1e194a1459bf2a0e6470fb44e459818d
fedPass <federica.passanante@gmail.com>
2019-12-10 15:37:49 +0100

    Jsnack 1

--- JSnack 1/main.js
+++ JSnack 1/main.js
@@ -2,68 +2,19 @@
 // indicando per ognuna varietà, peso e lunghezza.
 // Calcola quanto pesano tutte le zucchine.
 
-var zucchine = [
-    {
-        'varietà':'asd',
-        'lunghezza' : lunghezza,
-        'peso' : peso
-    },
-    {
-        'varietà':'asd',
-        'lunghezza' : lunghezza,
-        'peso' : peso
-    },
-    {
-        'varietà':'asd',
-        'lunghezza' : lunghezza,
-        'peso' : peso
-    },
-    {
-        'varietà':'asd',
-        'lunghezza' : lunghezza,
-        'peso' : peso
-    },
-    {
-        'varietà':'asd',
-        'lunghezza' : lunghezza,
-        'peso' : peso
-    },
-    {
-        'varietà':'asd',
-        'lunghezza' : lunghezza,
-        'peso' : peso
-    },
-    {
-        'varietà':'asd',
-        'lunghezza' : lunghezza,
-        'peso' : peso
-    },
-    {
-        'varietà':'asd',
-        'lunghezza' : lunghezza,
-        'peso' : peso
-    },
-    {
-        'varietà':'asd',
-        'lunghezza' : lunghezza,
-        'peso' : peso
-    },
-    {
-        'varietà':'asd',
+var pesoTot = 0;
+var zucchine = [];
+for (var i = 0; i < 10; i++) {
+    var lunghezza = generaRandom(1,20);
+    var peso = generaRandom(1,100);
+    var object = {
+        'varietà':'abc',
         'lunghezza' : lunghezza,
         'peso' : peso
     }
-]
-for (var i = 0; i < 10; i++) {
-    array[i]
-}
-var pesoTot = 0;
-for (var i = 0; i < zucchine.length; i++) {
-    var lunghezza = generaRandom(1,20);
-    var peso = generaRandom(1,100);
-    zucchine[i].peso = peso;
-    zucchine[i].lunghezza = lunghezza;
-    console.log('Il peso della zucchina numero ' + ([i]+1) + ' è ' + zucchine[i].peso);
+    zucchine.push(object);
+    var posizione = parseInt([i]) + 1
+    console.log('Il peso della zucchina numero ' + posizione + ' è ' + zucchine[i].peso);
     pesoTot += zucchine[i].peso;
 }
 console.log('Il peso totale è ' + pesoTot);
