05472cb40166e66ba999382f90379472c8b7ca81
fedPass <federica.passanante@gmail.com>
2019-12-10 17:29:44 +0100

    JSnack5

--- JSnack 5/main.js
+++ JSnack 5/main.js
@@ -3,3 +3,29 @@
 // massimo quanto il numero di elementi dell’array).
 // La funzione ritornerà un nuovo array con i valori che
 // hanno la posizione compresa tra “a” e “b”
+
+var array = [];
+var a = 4;
+var b = 8;
+var newArray = [];
+
+for (var i = 0; i < 10; i++) {
+    var element = generaRandom(1,50);
+    array.push(element)
+}
+console.log(array);
+
+position(array,a,b);
+
+function position(array,a,b) {
+    for (var i = a; i < b; i++) {
+        var current_element = array[i];
+        newArray.push(current_element)
+    }
+    console.log(newArray);
+    return newArray;
+}
+
+function generaRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
