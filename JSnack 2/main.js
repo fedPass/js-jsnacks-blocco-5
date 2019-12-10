ab5b31368f3fbfd96cdbe95eaf42469e0117c405
fedPass <federica.passanante@gmail.com>
2019-12-10 15:54:55 +0100

    JSnack2

--- JSnack 2/main.js
+++ JSnack 2/main.js
@@ -1,2 +1,19 @@
 // Scrivi una funzione che accetti una stringa come
 // argomento e la ritorni girata (es. Ciao -> oaiC)
+var text = prompt('Inserisci parola');
+var reverse = reverseString(text);
+console.log('Parola al contrario: ' + reverse);
+
+function reverseString(text) {
+    //leggo la parola
+    console.log(text);
+    //divido le lettere
+   var split = text.split('');
+   console.log(split);
+   //le inverto di posizione
+   var mirror = split.reverse();
+   console.log(mirror);
+   //le riunisco in una parola
+   var reverse = mirror.join('');
+   return reverse
+}
