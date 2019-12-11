537b9ff9bd6b3636aa28f3794f162725cec54755
fedPass <federica.passanante@gmail.com>
2019-12-11 10:28:38 +0100

    corretti parametri della funzione, usati più generici

--- JSnack 4/main.js
+++ JSnack 4/main.js
@@ -5,39 +5,39 @@
 
 var letter = ['a','b','c','d','e'];
 var number = [1,2,3];
-var mixed = [];
 
 mixElement(letter, number);
 
-function mixElement(letter,number){
-    var interation = Math.min(letter.length, number.length);
+function mixElement(array1, array2){
+    var mixed = [];
+    var interation = Math.min(array1.length, array2.length);
 
     for (var i = 0; i < interation; i++) {
         //prendi primo elemento di letter
-        var current_letter = letter[i];
+        var current_letter = array1[i];
         mixed.push(current_letter)
         //prendi primo elemento di number
-        var current_number = number[i];
+        var current_number = array2[i];
         mixed.push(current_number)
     }
-    console.log('La sequenza mixata è ' + mixed);
+    console.log('La sequenza mixata dello stesso numero di elementi degli array è ' + mixed);
     //se sono di lunghezza diversa dalla metà allora c'è ne sono altre da inserire
-    if ((letter.length > (mixed.length / 2)) || (number.length > (mixed.length / 2))) {
+    if ((array1.length > (mixed.length / 2)) || (array2.length > (mixed.length / 2))) {
         for (var i = 0; i < letter.length; i++) {
             //se non sono incluse
-            if (!mixed.includes(letter[i])){
+            if (!mixed.includes(array1[i])){
                 //includile
-                mixed.push(letter[i])
+                mixed.push(array1[i])
             }
         }
-        for (var i = 0; i < number.length; i++) {
+        for (var i = 0; i < array2.length; i++) {
             //se non sono incluse
-            if (!mixed.includes(number[i])){
+            if (!mixed.includes(array2[i])){
                 //includile
-                mixed.push(number[i]);
+                mixed.push(array2[i]);
             }
         }
     }
-    console.log(mixed);
+    console.log('La sequenza mixata finale è ' + mixed);
     return mixed;
 }
